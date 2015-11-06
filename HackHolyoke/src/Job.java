9bf4e809b3d11996ecc7a28d0e7391d146e566f8
sgrosser <stefan.grosser1@gmail.com>
2015-11-06 18:40:37 -0500

    added hardDueDate field to Job class. This is when the assignment is due

--- HackHolyoke/src/Job.java
+++ HackHolyoke/src/Job.java
@@ -7,20 +7,24 @@ public class Job {
 	public int ID;
 	public int expectedDuration;
 	public String subject;
-	public int startTime;
+	public int hardDueDate; //date assignment/event is due
+	public int actualStartTime;//user entered when starting
+	public int actualEndTime;
+	public int startTime; //assigned by scheduler
 	public int endTime;
 	public int weightedImportance = 10; //how important to finish on time. Defaults to max = 10
 	
-	public Job(int expectedDuration, String subject, int ID){
+	public Job(int expectedDuration, String subject, int ID, int hardDueDate){
 		this.expectedDuration = expectedDuration;
 		this.subject = subject;
+		this.hardDueDate = hardDueDate;
 		if(!Job.subject_finishTimeAcc.containsKey(subject)) Job.subject_finishTimeAcc.put(subject, 1);
 		this.ID = ID;
 		
 	}
 	
-	public Job(int expectedDuration, String subject, int ID, int weightedImportance){
-		this(expectedDuration, subject, ID);
+	public Job(int expectedDuration, String subject, int ID, int hardDueDate, int weightedImportance){
+		this(expectedDuration, subject, ID, hardDueDate);
 		this.weightedImportance = weightedImportance;
 	}
 	//TODO
